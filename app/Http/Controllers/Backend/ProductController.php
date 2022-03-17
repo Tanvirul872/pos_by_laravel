9a756c3874a41e40fa7ae99bc4ed146f780ec56f
Tanvirul872 <64800533+Tanvirul872@users.noreply.github.com>
2022-03-17 18:20:27 +0600

    change

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\backend;
+namespace App\Http\Controllers\backend; 
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -53,19 +53,34 @@ class ProductController extends Controller
 
 
     public function edit($id){
-        $editData =Product::find($id);
-        return view('backend.product.edit-product',compact('editData'));
+        $data['editData']  =Product::find($id);        
+        $data['suppliers'] = Supplier::all();
+        $data['brands'] = Brand::all();
+        $data['units'] = Unit::all();
+        $data['categories'] = Category::all();
+
+        return view('backend.product.edit-product',$data);
 
     }
 
     public function update(Request $request,$id){
 
-        $product = Product::find($id);
+        $product = Product::find($id); 
+        $product->supplier_id = $request->supplier_id;
+        $product->unit_id = $request->unit_id;
+        $product->brand_id = $request->brand_id;
+        $product->category_id = $request->category_id;
         $product->name = $request->name;
-        $product->mobile_no = $request->mobile_no;
-        $product->email = $request->email;
-        $product->address = $request->address;
-        $product->updated_by = Auth::user()->id;
+        $product->product_code = $request->product_code;
+        $product->product_price = $request->product_price;
+        $product->product_cost = $request->product_cost;
+        $product_profit= ($product->product_price-$product->product_cost);
+        $product->product_profit = $product_profit;
+        $profit_percent = (($product_profit/$product->product_cost)*100);
+        $product->product_profit_percent = $profit_percent;
+        $product->product_details = $request->product_details;
+        $product->product_img = $request->product_img;
+        $product->created_at = Auth::user()->id;
         $product->save();
         return redirect()->route('products.view')->with('success','Data updated successfully');
 
--- resources/views/backend/product/edit-product.blade.php
+++ resources/views/backend/product/edit-product.blade.php
@@ -21,23 +21,78 @@
                         <div class="card">
                             <div class="card-header">
                                 <h3>
-                                 Edit Brand
-                                    <a class="btn btn-success float-right" href="{{route('brands.view')}}"> View Brand</a>
+                                    Edit product
+                                    <a class="btn btn-success float-right" href="{{route('products.view')}}"> View product </a>
                                 </h3>
 
                                 <div class="card-body">
 
-                                    <form method="post" action="{{route('brands.update',$editData->id)}}" id="myForm">
+               
+                                    
+                                    <form method="post" action="{{route('products.update',$editData->id)}}" id="myForm">
                                         {{csrf_field()}}
 
                                         <div class="form-group">
 
-                                            <label> Supplier name </label>
-                                            <input type="text" name="name" class="form-control" id="name" value="{{$editData->name}}"  placeholder="Enter category name"> <br/>
+                                            <label> Product name </label>
+                                            <input type="text" name="name" class="form-control" value="{{$editData->name}}"  placeholder="Enter product name"> <br/>
 
+                                            <label> Product Code </label>
+                                            <input type="text" name="product_code" class="form-control" value="{{$editData->product_code}}"  placeholder="Enter product code"> <br/>
+
+
+                                            <label for="supplier_id"> Supplier name </label>
+                                            <select name="supplier_id" class="form-control">
+                                                <option value="">Select Supplier</option>
+                                                @foreach($suppliers as $supplier) 
+                                                <option value="{{$supplier->id}}" @if($editData->supplier_id == $supplier->id) selected @endif> {{$supplier->name}}  </option>
+                                                 @endforeach
+                                            </select>
+
+                                            <label for="brand_id"> Brand name </label>
+                                            <select name="brand_id" class="form-control">
+                                                <option value="">Select Brand</option>
+                                                @foreach($brands as $brand)
+                                                    <option value="{{$brand->id}}" @if($editData->brand_id == $brand->id) selected @endif> {{$brand->name}}  </option>
+                                                @endforeach
+                                            </select>
+
+                                            <label for="unit_id"> Unit name </label>
+                                            <select name="unit_id" class="form-control">
+                                                <option value="">Select Unit</option>
+                                                @foreach($units as $unit)
+                                                    <option value="{{$unit->id}}" @if($editData->unit_id == $unit->id) selected @endif> {{$unit->name}}  </option>
+                                                @endforeach
+                                            </select>
+
+                                            <label for="category_id"> Category name </label>
+                                            <select name="category_id" class="form-control">
+                                                <option value="">Select Category</option>
+                                                @foreach($categories as $category)
+                                                    <option value="{{$category->id}}" @if($editData->category_id == $category->id) selected @endif> {{$category->name}}  </option>
+                                                @endforeach
+                                            </select>
+
+
+
+
+                                            <label> Product Price </label>
+                                            <input type="text" name="product_price" class="form-control" value="{{$editData->product_price}}"  placeholder="Enter product price"> <br/>
+
+                                            <label> Product Cost </label>
+                                            <input type="text" name="product_cost" class="form-control" value="{{$editData->product_cost}}"  placeholder="Enter product cost"> <br/>
+
+                                            <label> Product Details </label>
+                                            <input type="text" name="product_details" class="form-control" value="{{$editData->product_details}}"  placeholder="Enter product details"> <br/>
+
+                                            <label> Product Image </label>
+                                            <input type="text" name="product_img" class="form-control"  value="{{$editData->product_img}}" placeholder="Enter product img url"> <br/>
+
+
+                                            <!-- <small style="color:red">  sdsd</small> -->
                                         </div>
 
-                                        <button type="submit" value="submit" class="btn btn-primary"> Update </button>
+                                        <button type="submit" value="submit" class="btn btn-primary"> submit </button>
                                     </form>
 
                                 </div>
